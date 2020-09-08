9dc56acd1f19354c0e205fcbe02aea23cc6bdb70
antonio.toche <antonio.toche@gmail.com>
2020-09-08 16:16:49 -0500

    info(excercises): rename some problems

--- python/BiggerIsGreater/biggerIsGreater.py
+++ python/BiggerIsGreater/biggerIsGreater.py
@@ -2,24 +2,22 @@ def biggerIsGreater(arr):
     arr = list(arr)
     i = len(arr) - 1
     while i > 0 and arr[i - 1] >= arr[i]:
-    	i -= 1
+        i -= 1
     if i <= 0:
-    	return "not answer"
-	
+        return "not answer"
+
     j = len(arr) - 1
     while arr[j] <= arr[i - 1]:
-    	j -= 1
+        j -= 1
     arr[i - 1], arr[j] = arr[j], arr[i - 1]
-	
-    arr[i : ] = arr[len(arr) - 1 : i - 1 : -1]
+
+    arr[i:] = arr[len(arr) - 1: i - 1: -1]
     result = ""
     for elements in arr:
-        result+=elements
+        result += elements
     return result
 
-        
 
-
-w ="hcdk" 
+w = "hcdk"
 
 print(biggerIsGreater(w))
